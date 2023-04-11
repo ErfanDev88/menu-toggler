7ef05824efd7cbb7e6a0a7ac7a1ff23f0237cd06
Mohamad Erfan <130228115+ErfanDev88@users.noreply.github.com>
2023-04-11 16:34:59 +0330

    Create Menu.jsx

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <ul className='mt-5 p-4 bg-gray-300 rounded-lg flex flex-col gap-y-2'>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About us</li>
+    </ul>
+    )
+}
+
+export default Menu
